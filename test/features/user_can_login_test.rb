9b5e8eaba17eccce2b6aa7453233c3c7c806e327
Byronj <cekcbj@gmail.com>
2015-09-26 16:08:55 -0500

    log out test

--- test/features/user_can_login_test.rb
+++ test/features/user_can_login_test.rb
@@ -1,9 +1,10 @@
 require "test_helper"
 
 class UserCanLoginTest < Capybara::Rails::TestCase
+
   test "userlogin" do
 
-    User.create first_name:'Byron', last_name: "Jones", email:"Byron@yahoo.com", password: "12345678"
+  me =  User.create first_name:'Byron', last_name: "Jones", email:"Byron@yahoo.com", password: "12345678"
 
     visit root_path
     fill_in 'email', with:'Byron@yahoo.com'
@@ -11,5 +12,22 @@ class UserCanLoginTest < Capybara::Rails::TestCase
 
     click_button "Log In"
     assert_content page, "Hello Byron"
+  end
+
+  test "userlogout" do
+
+    me =  User.create first_name:'Byron', last_name: "Jones", email:"Byron@yahoo.com", password: "12345678"
+
+    visit root_path
+    fill_in 'email', with:'Byron@yahoo.com'
+    fill_in 'password', with:'12345678'
+
+    click_button "Log In"
+    assert_content page, "Hello Byron"
+    click_link "Log Out"
+    assert_content page, "You have been logged out"
+    assert_content page, "Sign Up"
+
+
   end
 end
